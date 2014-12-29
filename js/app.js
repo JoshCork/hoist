c34f61478111aaef97b658caf2854601d55868e5
JoshCork <joshua.cork@gmail.com>
2014-12-29 11:36:26 -0700

    added scoring
    Added a scoreboard object and moved the life count and score keeping
    into that object.  Still need to create an end game and update the text
    on the scoreboard because it isn't rendering properly... it isn't
    clearing it out each time.

--- js/app.js
+++ js/app.js
@@ -1,16 +1,48 @@
+var ScoreBoard = function() {
+    this.livesText = "Lives: "
+    this.gameScoreText = " Score: "
+    this.score = 0;
+    this.lives = 5;
+
+}
+
+ScoreBoard.prototype.render = function() {
+    ctx.font = "36pt Impact";
+    ctx.textAlign = "left";
+    ctx.strokeStyle = 'black';
+    ctx.line = 3;
+    ctx.fillStyle = 'white';
+
+    ctx.fillText(this.livesText + this.lives + this.gameScoreText + this.score, 0, 50);
+    ctx.strokeText(this.livesText + this.lives + this.gameScoreText + this.score, 0, 50);
+    //console.log("i've rendered the scoreboard");
+}
+
+ScoreBoard.prototype.update = function() {
+    // update the scoreboard here. 
+}
+
+ScoreBoard.prototype.increment = function() {
+    this.score = this.score + 100;
+}
+
+ScoreBoard.prototype.decrement = function() {
+    this.lives = --this.lives;
+}
+
 // Enemies our player must avoid
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-    this.spawnRange = [60,140,220];
-    this.speedRange = [40,500];  
+    this.spawnRange = [60, 140, 220];
+    this.speedRange = [40, 500];
 
     this.x = 0;
-    this.y = this.spawnRange[getRandomInt(0,2)]; 
+    this.y = this.spawnRange[getRandomInt(0, 2)];
 
     this.boardLoc = getBoardLoc(this.x, this.y);
-    this.speed = getRandomInt(this.speedRange[0],this.speedRange[1]);
-    
+    this.speed = getRandomInt(this.speedRange[0], this.speedRange[1]);
+
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
@@ -26,16 +58,14 @@ Enemy.prototype.update = function(dt) {
     // which will ensure the game runs at the same speed for
     // all computers.
     if (this.x < 600) {
-        this.x = this.x + (this.speed*dt);
-    }
-    else
-    {
+        this.x = this.x + (this.speed * dt);
+    } else {
         this.x = -50;
-        this.y = this.spawnRange[getRandomInt(0,3)]; 
-        this.speed = getRandomInt(this.speedRange[0],this.speedRange[1]);
+        this.y = this.spawnRange[getRandomInt(0, 3)];
+        this.speed = getRandomInt(this.speedRange[0], this.speedRange[1]);
     }
 
-    this.boardLoc = getBoardLoc(this.x,this.y);
+    this.boardLoc = getBoardLoc(this.x, this.y);
     //console.log("EnemyLocation: " + this.boardLoc);
 }
 
@@ -43,7 +73,7 @@ Enemy.prototype.update = function(dt) {
  source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
  */
 function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
+    return Math.floor(Math.random() * (max - min)) + min;
 }
 
 // Draw the enemy on the screen, required method for game
@@ -57,12 +87,14 @@ Enemy.prototype.render = function() {
 
 var Player = function() {
     this.sprite = 'images/char-boy.png';    
-    
-    this.x = 200;
-    this.y = 380;
+    this.respawnLoc = [200, 380];
+
+    this.x = this.respawnLoc[0];
+    this.y = this.respawnLoc[1];
+
     this.vHops = 81;
     this.hHops = 100;
-    this.boardLoc = getBoardLoc(this.x,this.y);
+    this.boardLoc = getBoardLoc(this.x, this.y);
     // generic class for a player.
 }
 
@@ -70,7 +102,7 @@ Player.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.boardLoc = getBoardLoc(this.x,this.y);
+    this.boardLoc = getBoardLoc(this.x, this.y);
     //console.log("playerLocation: " + this.boardLoc);
 }
 
@@ -79,36 +111,60 @@ Player.prototype.render = function() {
 
 }
 
+Player.prototype.respawn = function() {
+    if (scoreboard.lives > 0) {
+        this.x = this.respawnLoc[0];
+        this.y = this.respawnLoc[1];
+        scoreboard.decrement();
+    } else {
+        // todo: call a function that ends the game
+    }
+}
+
+Player.prototype.achievement = function() {
+    this.y = 380;
+    scoreboard.increment();
+    
+}
+
 Player.prototype.handleInput = function(input) {
-    console.log(input);   
     //todo: need to figure out how to not make these values hard coded as limits. 
     switch (input) {
         case 'up':
-            if (this.y > 57 ) { this.y = this.y - this.vHops; } else { this.y = 380 }
-            console.log(this.y)
+            if (this.y > 57) {
+                this.y = this.y - this.vHops;
+            } else {
+                player.achievement();
+            }
             break;
         case 'down':
-            if (this.y < 380) { this.y = this.y + this.vHops; } else { /* do nothing */ }
+            if (this.y < 380) {
+                this.y = this.y + this.vHops;
+            } else { /* do nothing */ }
             break;
         case 'left':
-            if (this.x > 0) { this.x = this.x - this.hHops; } else { /* do nothing */ }
+            if (this.x > 0) {
+                this.x = this.x - this.hHops;
+            } else { /* do nothing */ }
             break;
         case 'right':
-            if (this.x < 400) { this.x = this.x + this.hHops; } else { /* do nothing */ }            
+            if (this.x < 400) {
+                this.x = this.x + this.hHops;
+            } else { /* do nothing */ }
             break;
     }
 
 
 }
 
-function getBoardLoc(x,y) {
+function getBoardLoc(x, y) {
     var myBoardLoc = [];
 
-    myBoardLoc.push(Math.ceil((x)/100));
-    myBoardLoc.push(Math.ceil((y)/81));
+    myBoardLoc.push(Math.ceil((x) / 100));
+    myBoardLoc.push(Math.ceil((y) / 81));
 
     return myBoardLoc;
-    
+
 }
 
 
@@ -118,10 +174,10 @@ function getBoardLoc(x,y) {
 var allEnemies = [];
 for (i = 0; i < 3; i++) {
     allEnemies.push(new Enemy());
-    console.log(allEnemies);
 }
 
 var player = new Player();
+var scoreboard = new ScoreBoard();
 
 
 
